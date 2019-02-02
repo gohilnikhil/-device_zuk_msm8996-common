9bce6d075013f8ffece78696788838b22799b6d4
LuK1337 <priv.luk@gmail.com>
2019-02-02 19:34:23 +0100

    msm8996-common: Add debug IMS props to workaround CarrierConfig restrictions
    We support IMS features on carriers not listed in the CarrierConfig,
    so let's use the props to enable it everywhere.
    
    Change-Id: Id3ad71d522a22048f7ad86d89695ad262361632d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -238,8 +238,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # VoLTE hax
 PRODUCT_PROPERTY_OVERRIDES += \
-   persist.dbg.volte_avail_ovr=1 \
-   persist.dbg.vt_avail_ovr=1
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1  \
+    persist.dbg.wfc_avail_ovr=1
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
