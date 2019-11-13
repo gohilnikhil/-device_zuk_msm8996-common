92cea697ea2183d8648fe5075d2b1cde1f18e5e3
Anushek Prasal <anushekprasal@gmail.com>
2019-11-14 01:48:54 +0530

    msm8996-common: Kang KeyHandler for tri-state-key from sdm845-common

--- msm8996.mk
+++ msm8996.mk
@@ -438,3 +438,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
+# Tri-state keys
+PRODUCT_PACKAGES += \
+    KeyHandler
